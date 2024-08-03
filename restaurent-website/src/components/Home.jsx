83bce949238a6e474d302dda8cbd5cbdf288a1cd
Ashen Kalhara <ashennkalhara@gmail.com>
2024-08-03 08:25:17 +0530

    Reservation done

--- restaurent-website/src/components/Home.jsx
+++ restaurent-website/src/components/Home.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+// src/components/Home.jsx
+import React, { useState } from "react";
 import Button from "../layouts/Button";
+import ReserveModal from "./ReserveModal";
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/home.jpg')] bg-cover bg-no-repeat ">
-      <div className=" w-full lg:w-2/3 space-y-5">
-        <h1 className=" text-backgroundColor font-semibold text-6xl">
-        Taste the Artistry in Every Dish
+    <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/home.jpg')] bg-cover bg-no-repeat ">
+      <div className="w-full lg:w-2/3 space-y-5">
+        <h1 className="text-backgroundColor font-semibold text-6xl">
+          Taste the Artistry in Every Dish
         </h1>
-        <p className=" text-backgroundColor">
-        Savor the flavors of our gourmet cuisine, where every dish is a masterpiece. 
-        Our chefs blend fresh ingredients with creative flair to bring you an unforgettable dining experience. Whether it's a hearty breakfast, a satisfying lunch, or a sumptuous dinner, 
-        delight your taste buds with our delectable offerings.
+        <p className="text-backgroundColor">
+          Savor the flavors of our gourmet cuisine, where every dish is a masterpiece.
+          Our chefs blend fresh ingredients with creative flair to bring you an unforgettable dining experience. Whether it's a hearty breakfast, a satisfying lunch, or a sumptuous dinner,
+          delight your taste buds with our delectable offerings.
         </p>
-          <Button title="Order Now" />
-        </div>
+        <Button title="Reserve Now" onClick={handleOpenModal} />
       </div>
+
+      <ReserveModal isOpen={isModalOpen} onClose={handleCloseModal} />
+    </div>
   );
 };
 
